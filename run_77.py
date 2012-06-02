f4d318ddb1ed01cca3c5dc675786ce7df67082b5
Christian Fobel <christian@fobel.net>
2012-06-02 11:19:46 -0400

    typo in  arch_file

--- run_77.py
+++ run_77.py
@@ -9,13 +9,12 @@ if __name__ == "__main__":
     SharcNetTrial._default_connection = SharcNetConnection()
 
     for net in nets:
-        np = path('$BENCHMARK_PATH') /  path(net.strip())
+        np = path('$BENCHMARK_PATH') / path(net.strip())
         print np
-        S = SharcNetTrial(params=[('netlist_file', np), ('arch_file', '$BENCHMARK_PATH/k4-1n.xml'), 
-                                    ('scaling_factor', '0.0'), ('stride_factor', '0.0')], time=120, 
-                                    priority=1, out_path='$PYVPR_RESULTS/77', 
+        S = SharcNetTrial(params=[('netlist_file', np), ('arch_file', '$BENCHMARK_PATH/k4-n1.xml'), 
+                                    ('scaling_factor', '0.0'), ('stride_factor', '0.0')], time=25, 
+                                    priority=1, out_path='$PYVPR_RESULTS/',
                                     exe_path='$PYVPR_EXPERIMENTS/77/sa_x_ga.py')
-       # S.make_output_dir()
-       # S.write_config()
-       # S.submit()
-
+        S.make_output_dir()
+        S.write_config()
+        S.submit()
