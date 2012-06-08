c0c9bcc3cc440dca005fd9bdaa8773ed45aa9ff5
Ryan Pattison <ryan.m.pattison@gmail.com>
2012-06-08 15:20:42 -0400

    added params to make mcnc OR uoft

--- freeze_all_trials.py
+++ freeze_all_trials.py
@@ -9,24 +9,32 @@ from filter import add_params
 
 if __name__ == "__main__":
 
-    if sys.argv[1] == '-gen':
-        mcnc = [path(n.strip()) for n in open("./mcnc.txt")]
-        uoft = [path(u.strip()) for u in open("./uoft.txt")]
+    mcnc = [path(n.strip()) for n in open("./mcnc.txt")]
+    uoft = [path(u.strip()) for u in open("./uoft.txt")]
+
+    name = '96_trial_'
+    if sys.argv[1] == '-mcnc':    
+        nets = mcnc
+        name += 'mcnc'
+    elif sys.argv[1] == '-uoft':
+        nets = uoft
+        name += 'uoft'
+    elif sys.argv[1] == '-all':
         nets = mcnc + uoft
-        net_info = yaml.load(path('./'\
-                'anneal-fast-mean_outer_iterations_and_runtimes.yml').bytes())
+        name += 'all'
+    name += '.shelve'
 
-        trial = shelve.open('96_trial_file.shelve', 'c')
+    net_info = yaml.load(path('./'\
+            'anneal-fast-mean_outer_iterations_and_runtimes.yml').bytes())
 
-        for net in nets:
-            for seed in range(10):
-                # Convert runtime to minutes with a minimum of 15 and
-                # factoring in cpu speed differences.
-                runtime = int(15 + (net_info[net.namebase]['runtime'] * 3 / 60))
-                run_count = net_info[net.namebase]['outer iter']
-                np = '$BENCHMARK_PATH/' + net
-                params=[('netlist_file', np), ('arch_file', '$BENCHMARK_PATH/k4-n1.xml'), 
-                        ('seed', seed), ('run_count', run_count), ('inner_num', 1)]
-                add_params(trial, dict(params))
+    trial = shelve.open(name, 'c')
 
-        trial.close()
+    for net in nets:
+        for seed in range(10):
+            run_count = net_info[net.namebase]['outer iter']
+            np = '$BENCHMARK_PATH/' + net
+            params=[('netlist_file', np), ('arch_file', '$BENCHMARK_PATH/k4-n1.xml'), 
+                    ('seed', seed), ('run_count', run_count), ('inner_num', 1)]
+            add_params(trial, dict(params))
+
+    trial.close()
